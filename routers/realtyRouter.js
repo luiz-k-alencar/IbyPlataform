15aa412e2d595d7cf02f394f9358a49393f8586c
Luiz Felipe <68920578+luiz-k-alencar@users.noreply.github.com>
2022-12-23 09:52:17 -0300

    Project publish

---
+++ routers/realtyRouter.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const realtyController = require("../controllers/realtyController");
+
+const router = express.Router();
+
+/**
+ * routes middleware
+ */
+router
+  .route("/")
+  .get(realtyController.getAllRealty)
+  .post(realtyController.createNewRealty);
+
+router
+  .route("/:id")
+  .get(realtyController.getRealty)
+  .patch(realtyController.updateRealty);
+
+module.exports = router;
